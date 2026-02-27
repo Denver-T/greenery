cda8fb55ca3316e3ed01d7e715556bd5b1681f39
noah.stanich@edu.sait.ca <noah.stanich@edu.sait.ca>
2026-02-27 10:22:38 -0700

    Implements rate limit

--- apps/api/src/monday.js
+++ apps/api/src/monday.js
@@ -1,11 +1,38 @@
-let query = '{ boards (limit:5) {name id} }';
+import {rateLimit} from 'express-rate-limit';
+
+const token = process.env.api_token;
+
+// Rate limit implementation
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    limit: 100
+});
+app.use(limiter);
+
+// Fetch boards by ID (Must use POST)
+
+let query = '{ boards {name id title text} }';
 fetch ("https://api.monday.com", {
   method: 'post',
   headers: {
     'Content-Type': 'application/json',
-    'Authorization' : 'MONDAY_BOARD_ID'
+    'Authorization' : token
   },
   body: JSON.stringify({ 'query' : query })
 })
 .then(res => res.json())
-.then(res => console.log(JSON.stringify(res, null, 2)));
+.then(data => console.log(data));
+
+// Delete board
+
+let del = delete_item(board_id);
+fetch ("https://api.monday.com", {
+  method: 'post',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization' : token
+  },
+  body: JSON.stringify({ 'query' : post })
+})
+.then(res => res.json())
+.then(data => console.log(data));
